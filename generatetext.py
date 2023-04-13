5fb186bb30768c0f5eedbc4f7b8d45cb06f3b959
David Burela <David.Burela@gmail.com>
2023-04-13 18:23:55 +1000

    a

---
+++ generatetext.py
@@ -0,0 +1,19 @@
+#!pip install langchain llama-cpp-python
+from langchain import OpenAI
+from langchain.llms import LlamaCpp
+
+## Cloud
+# model = OpenAI()
+
+## Edge
+# model = LlamaCpp(model_path="./models/gpt4all-lora-quantized-new.bin", verbose=True, n_threads=16)
+# model = LlamaCpp(model_path="./models/ggml-vicuna-7b-4bit-rev1.bin", verbose=True, n_threads=16)
+model = LlamaCpp(model_path="./models/ggml-vicuna-13b-4bit-rev1.bin", verbose=True, n_threads=16)
+
+
+# Generate text
+prompt = "Once upon a time, "
+
+print("Sending prompt: " + prompt)
+response = model(prompt)
+print(response)
