ec0975d106df85c11925e8275dcfc0b8331d89b7
David Burela <David.Burela@gmail.com>
2023-06-02 12:41:30 +1000

    cleaning sql example

--- sql-question.py
+++ sql-question.py
@@ -23,12 +23,12 @@ driver = "ODBC+Driver+18+for+SQL+Server"
 
 ### LLM model ###
 ## Cloud
-# model = OpenAI()
+model = OpenAI()
 
 ## Edge
 # model = LlamaCpp(model_path="./models/llama-7b-ggml-v2-q4_0.bin", verbose=True, n_threads=8, n_gpu_layers=26)
 # model = LlamaCpp(model_path="./models/stable-vicuna-13B-ggml_q4_0.bin", verbose=True, n_threads=8, n_gpu_layers=10)
-model = LlamaCpp(model_path="./models/koala-7B.ggml.q4_0.bin", verbose=True, n_threads=8, n_gpu_layers=26)
+# model = LlamaCpp(model_path="./models/koala-7B.ggml.q4_0.bin", verbose=True, n_threads=8, n_gpu_layers=26)
 
 # Create connection string
 params = urllib.parse.quote_plus(f"DRIVER={{{driver}}};SERVER={server};DATABASE={database_name};UID={username};PWD={password};TrustServerCertificate=yes;Encrypt=yes")
