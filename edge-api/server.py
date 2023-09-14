c4d8f17ed39a5ac68b936c26866bba04f63dbf4c
Jamie D <jamdalt@microsoft.com>
2023-09-14 19:19:14 +0100

    liveshare changes

--- edge-api/server.py
+++ edge-api/server.py
@@ -9,19 +9,18 @@ from werkzeug.utils import secure_filename
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
+from langchain.llms import LlamaCpp
 
 app = Flask(__name__, static_folder="../edge-ui/dist/", static_url_path="/")
 CORS(app)
 
-model = LlamaCppEmbeddings(model_path="./models/llama-2-7b.Q4_K_M.gguf", n_threads=8)
-
+model = LlamaCpp(model_path="./models/llama-2-7b.Q4_K_M.gguf", n_threads=8)
+embeddings = LlamaCppEmbeddings(model_path="./models/llama-2-7b.Q4_K_M.gguf", n_threads=8)
 
 currentDocumentName = ""
 currentDocumentPath = ""
 qaChain = None
-
-
 
 @app.route('/document', methods = ['GET'])
 def get_document_details():
@@ -45,11 +44,12 @@ def upload_file():
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
-    print(f"{len(texts)} chunks")
     
-    docSearch = Chroma.from_documents(texts, model)
+    print(f"Found {len(texts)} chunks")
+    
+    docSearch = FAISS.from_documents(texts, embeddings)
 
-    # Create a chain that uses the OpenAI LLM and HNSWLib vector store.
+    # Create a chain that uses the LlamaCPP LLM and FAISS vector store.
     qaChain = RetrievalQA.from_chain_type(llm=model, chain_type="stuff", retriever=docSearch.as_retriever())
 
 
